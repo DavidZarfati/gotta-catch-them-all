66fbaecb30521699770c5cfd5b48d144af33ffba
DavidZarfati <zarda2001@gmail.com>
2025-11-17 11:34:08 +0100

    feat:passaggio alle funzioni

--- js/script.js
+++ js/script.js
@@ -3,26 +3,37 @@ axios.get("https://pokeapi.co/api/v2/pokemon")
         const pokemonList = document.getElementById("pokemon-list");
         const pokemonArray = resp.data.results;
         pokemonArray.forEach((pokemon) => {
-            const item = document.createElement("li");
-            item.innerHTML = pokemon.name;
-            item.classList.add("list-group-item");
-            item.addEventListener("click", function () {
-                axios
-                    .get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-                    .then(function (resp) {
-                        const dettagli = resp.data;
-                        console.log(dettagli);
+            const listItem = createItem(pokemon)
+            pokemonList.append(listItem);
+        });
+    });
 
-                        const cardElem = document.querySelector('.card');
-                        cardElem.innerHTML = `
+function printCard(dettagli) {
+    const cardElem = document.querySelector('.card');
+    cardElem.innerHTML = `
                             <img class="card-image w-25" src="${dettagli.sprites.front_default}" alt="" />
                             <div class="card-body">
                                 <h2>${dettagli.name}</h2>
                                 <p>Peso: ${dettagli.weight}</p>
                             </div>
                         `;
-                    });
-            });
-            pokemonList.append(item);
-        });
+}
+
+function createItem(pokemon) {
+    const item = document.createElement("li");
+    item.innerHTML = pokemon.name;
+    item.classList.add("list-group-item");
+    item.addEventListener("click", function () {
+        getPokemonDetails(pokemon.name)
     });
+    return item
+}
+
+function getPokemonDetails(name) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then(function (resp) {
+        const dettagli = resp.data;
+        printCard(dettagli);
+        console.log(dettagli);
+
+    });
+}
